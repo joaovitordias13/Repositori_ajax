fa25417fb64ac9370aef50a497e68efbf0bd7c44
Joao Vitor Ribeiro <joaovitordias.2b@gmail.com>
2023-08-23 19:14:12 -0300

    Exercicio Ajax

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const name = document.getElementById('#name');
+    const avatar = document.getElementById('#avatar');
+    const username = document.getElementById('#username');
+    const respositorios = document.getElementById('#respositorios');
+    const seguidores = document.getElementById('#seguidores');
+    const seguindo = document.getElementById('#seguindo');
+    const link = document.getElementById('#link');
+    const api = "https://api.github.com/users/joaovitordias13"
+
+    fetch(api)
+    .then(function(resposta) {
+        return resposta.json();
+    })
+    .then(function(json) {
+        name.innerText = json.name;
+        username.innerText = json.login;
+        avatar.src = json.avatar_url;
+        respositorios.innerText = json.public_repos;
+        seguidores.innerText = json.followers;
+        seguindo.innerText = json.following;
+        link.href = json.htm_url;
+    })
+})
